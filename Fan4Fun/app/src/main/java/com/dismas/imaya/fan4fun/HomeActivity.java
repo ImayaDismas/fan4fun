d85402297cc1877cb09f9bdefeb6110fe73d4387
ImayaDismas <imayadismas@gmail.com>
2016-03-27 00:56:27 +0300

    Added content to home activity

--- Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/HomeActivity.java
+++ Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/HomeActivity.java
@@ -1,7 +1,17 @@
 package com.dismas.imaya.fan4fun;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by imaya on 3/26/16.
  */
-public class HomeActivity {
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
+
