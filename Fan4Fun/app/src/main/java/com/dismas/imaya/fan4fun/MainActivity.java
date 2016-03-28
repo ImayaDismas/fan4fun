b939d0009fbdb6e91dbe256ed08d5b32d6a9d63d
ImayaDismas <imayadismas@gmail.com>
2016-03-28 08:19:20 +0300

    Added all activities for player

--- Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/MainActivity.java
+++ Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
         addListenerOnButton();
         addListenerOnButton1();
     }
