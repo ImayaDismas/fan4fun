2eb565875188ec0b03ff592349237863f3daa64c
ImayaDismas <imayadismas@gmail.com>
2016-03-28 07:00:56 +0300

    added the dependencies

--- Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/HomeActivity.java
+++ Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/HomeActivity.java
@@ -1,17 +1,60 @@
 package com.dismas.imaya.fan4fun;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by imaya on 3/26/16.
  */
 public class HomeActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+        addListenerOnButton();
+        addListenerOnButton1();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, MainSoundCloud.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton1() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, VideoItemListActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
-
