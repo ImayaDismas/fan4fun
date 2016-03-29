16ae9f62aaccd2b02421a5bfb642fa519b0e54d9
ImayaDismas <imayadismas@gmail.com>
2016-03-29 05:45:33 +0300

    Added activities for the buttons and the listeners

--- Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/MainActivity.java
+++ Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/MainActivity.java
@@ -1,13 +1,58 @@
 package com.dismas.imaya.fan4fun;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+        addListenerOnButton1();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, MainSoundCloud.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    public void addListenerOnButton1() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, VideoItemListActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
---
+++ Fan4Fun/app/src/main/java/com/dismas/imaya/fan4fun/VideoItemListActivity.java
@@ -0,0 +1,7 @@
+package com.dismas.imaya.fan4fun;
+
+/**
+ * Created by imaya on 3/29/16.
+ */
+public class VideoItemListActivity {
+}
